93742af16b209906a2cbf250bc0b722f53bce3ee
kobunhada <android.kobunhada@gmail.com>
2021-03-08 12:44:55 +0900

    Added comment.

--- src/main/kotlin/com/kobunhada/kotlin/example/eventdriven/Item.kt
+++ src/main/kotlin/com/kobunhada/kotlin/example/eventdriven/Item.kt
@@ -5,6 +5,7 @@ import java.beans.PropertyChangeListener
 
 class Item {
     var name: String = ""
+    // use backing fields
     var owner: String = ""
         set(value) {
             field = value
